3a132bc6d4c62cb2eb4a86fb153657e9987ae647
蛮吉 <hanabi.almost@gmail.com>
2026-06-23 10:23:22 +0800

    chore: 更新版本号至 3.0.1
    - 将 versionCode 从 300 更新为 301
    - 将 versionName 从 3.0 更新为 3.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "app.smartisanmusic.revived"
         minSdk = 31
         targetSdk = 36
-        versionCode = 300
-        versionName = "3.0"
+        versionCode = 301
+        versionName = "3.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
